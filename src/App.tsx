ba1769030fefbd9a988e83643b56269ed2a89b9d
Erwan SINCK <erwan.sinck12@gmail.com>
2025-03-21 14:17:05 +0100

    ouai ouai

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import GlassIcons from './componants/GlassIcons'
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+import { useState } from "react";
 
 
 function App() {
@@ -15,6 +18,8 @@ function App() {
     { icon: <FaLinkedin />, color: 'linkedin', label: 'linkedin', url:"https://linkedin.erwansinck.com" },
     { icon: <FaSquareXTwitter />, color: 'x', label: 'x', url:"https://x.erwansinck.com" },
   ];
+
+  const [visible, setVisible] = useState<boolean>(false)
 
 
   return (
@@ -79,6 +84,49 @@ function App() {
       <div className="absolute text-white left-[50%] transform translate-x-[-50%] top-150">
         <GlassIcons items={items} className="custom-class"/>
       </div>
+
+     
+      <Dialog header="Mentions Légales erwansinck.com" visible={visible} style={{ width: '50vw' }} onHide={() => {if (!visible) return; setVisible(false); }} className="bg-gray-300 p-6 rounded-2xl">
+          <h1 className="mt-5 mb-2">1. Éditeur du Site</h1>
+          <p>Nom du propriétaire : Erwan SINCK</p>
+          <p>Email : <a href="mailto:erwan.sinck@ynov.com">erwan.sinck@ynov.com</a></p>
+          <p>Site web : <a href="https://erwansinck.com">https://erwansinck.com</a></p>
+          <p>Date de publication : 2025</p>
+
+          <h1 className="mt-5 mb-2">2. Hébergement</h1>
+          <p>Ce site est hébergé sur un nœud appartenant à l'éditeur, avec un reverse proxy installé sur un autre nœud hébergé chez OVH.</p>
+
+          <h1 className="mt-5 mb-2">3. Technologies utilisées</h1>
+          <p>Ce site est développé en React</p>
+          <p>Hébergé via Docker, avec un reverse proxy Nginx également dockerisé</p>
+
+          <h1 className="mt-5 mb-2">4. Licence et Utilisation du Contenu</h1>
+          <p>Sauf mention contraire, l’ensemble des contenus présents sur ce site (textes, images, graphismes, logo, icônes, etc.) sont mis à disposition sous la Licence MIT.</p>
+          <p>Cela signifie que toute personne est libre de :</p>
+          <p>- Copier, modifier et redistribuer le contenu du site, même à des fins commerciales,</p>
+          <p>- Sans obligation de mentionner l’auteur,</p>
+          <p>- Sans nécessiter d’autorisation préalable.</p>
+          <p>L’utilisateur reconnaît toutefois que le contenu est fourni "tel quel", sans garantie de mise à jour ou de support. L’éditeur du site décline toute responsabilité quant à l’utilisation faite du contenu mis à disposition.</p>
+          
+          <h1 className="mt-5 mb-2">5. Cookies</h1>
+          <p>Ce site n'utilise pas de cookies.</p>
+
+          <h1 className="mt-5 mb-2">6. Responsabilité</h1>
+          <p>L'éditeur du site met tout en œuvre pour assurer l'exactitude des informations publiées, mais ne saurait être tenu responsable des erreurs ou omissions. L'utilisation des informations disponibles sur ce site se fait sous la responsabilité exclusive de l'utilisateur.</p>
+
+          <h1 className="mt-5 mb-2">7. Données personnelles</h1>
+          <p>Ce site ne collecte aucune donnée personnelle à des fins commerciales. Pour toute question ou demande relative aux données personnelles, vous pouvez contacter l'éditeur à l'adresse mentionnée ci-dessus.</p>
+
+          <h1 className="mt-5 mb-2">8. Droit applicable</h1>
+          <p>Les présentes mentions légales sont soumises au droit français. En cas de litige, les tribunaux français seront seuls compétents.</p>
+
+      </Dialog>
+      <Button 
+        label="Mentions Légales" 
+        className="fixed top-3 left-3 text-black px-3 "
+        onClick={() => setVisible(true)}
+      />
+
 
     </div>
   )
